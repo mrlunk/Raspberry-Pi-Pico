be1665fd955d6593a5374bdd07f74c932f3f1234
Peter Lunk <mrlunk@gmail.com>
2023-02-12 17:40:07 +0100

    Update Wifi_NTP_TimeSynced_Clock.py

--- Wifi_NTP_TimeSynced_Clock.py
+++ Wifi_NTP_TimeSynced_Clock.py
@@ -1,6 +1,19 @@
 # WORK IN PROGRESS - Under Construction
 # Script by: Peter Lunk 
 
+"""
+This MicroPython script is for Raspberry Pi Pico W (Wifi) device
+that connects to a Wi-Fi network and synchronizes its time with 
+a NTP (Network Time Protocol) server. 
+The script first sets up a connection to the Wi-Fi network using
+the specified ssid (network name) and password, and then sets
+the device's time using the ntptime.settime() function. 
+The device continuously retrieves the current time from internal
+clock and displays it along with the current date and day of the
+year on the serial monitor every second.
+The script synchronizes the time with NTP every day at midnight.
+"""
+
 import network
 import socket
 import time
