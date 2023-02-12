7c8f0e50c866cf3289453e151fa60e2154627525
Peter Lunk <mrlunk@gmail.com>
2023-02-12 13:27:53 +0100

    Create Wifi_NTP_TimeSynced_Clock.py
    A MicroPython script for RaspberryPi Pico W
    A 24 hour clock and date time display (in serial monitor)
    that synchronises time with NTP server at midnight daily.

---
+++ Wifi_NTP_TimeSynced_Clock.py
@@ -0,0 +1,75 @@
+import network
+import socket
+import time
+import struct
+import ntptime
+from machine import Pin
+
+led = Pin("LED", Pin.OUT)
+
+x = 1
+Hour = ""
+Minute = ""
+Seconds = ""
+Maanden = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Okt","Nov","Dec"]
+WeekDagen = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+
+TimeCheck = 300
+
+ssid = 'LunkTech3'
+password = 'DoeMijDieMaar'
+
+def connect_wifi():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+
+    max_wait = 10
+    while max_wait > 0:
+        if wlan.status() < 0 or wlan.status() >= 3:
+            break
+        max_wait -= 1
+        print('waiting for connection...')
+        time.sleep(1)
+
+    if wlan.status() != 3:
+        raise RuntimeError('network connection failed')
+    else:
+        print('connected')
+        status = wlan.ifconfig()
+        print( 'ip = ' + status[0] )
+    
+#--------------------------------------------------------------------------------
+connect_wifi()
+
+ntptime.settime()
+
+Status = wlan.active()
+print (Status)
+
+while True:
+    Year = time.localtime()[0]
+    Maand = (time.localtime()[1])-1
+    Day = time.localtime()[2]
+    Hour = time.localtime()[3]
+    Minute = time.localtime()[4]
+    Second = time.localtime()[5]
+    DayOTWeek = (time.localtime()[6])
+    DayNumber = time.localtime()[7]
+    
+    # ---- Print Date time info to Serial Monitor -------------------------------------
+    print("Time: ",Hour,":",Minute,":",Second)
+    print(WeekDagen[DayOTWeek],Maanden[Maand],Day,Year)
+    print("Day Nuber: ",DayNumber) 
+    print("")
+    time.sleep(1)
+    
+    # ---- Sync with NTP time server once per Day at Midnight:
+    if Hour == 0:
+        if Minute == 0:
+            if Second == 0:
+                print("")
+                ntptime.settime()
+                print("")
+                print ("Time Synced at 00:00:00 ....")
+                print("")
