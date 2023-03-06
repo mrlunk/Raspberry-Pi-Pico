ff9842a7d5895d63a1865f9aee87d926720c16f0
Peter Lunk <mrlunk@gmail.com>
2023-03-06 11:16:46 +0100

    Update Button_press_test.py

--- Binary_LED_Clock/Button_press_test.py
+++ Binary_LED_Clock/Button_press_test.py
@@ -7,6 +7,15 @@ Script by: MrLunk
 https://github.com/mrlunk/Raspberry-Pi-Pico/
 """
 
+"""
+This code defines a button press handler function that detects when a button connected
+to pin 27 is pressed and prevents double-clicks by ignoring button presses that occur
+within 500 milliseconds of the last press.
+
+Script by: MrLunk
+https://github.com/mrlunk/Raspberry-Pi-Pico/
+"""
+
 import machine
 import time
 
@@ -15,21 +24,27 @@ button = machine.Pin(27, machine.Pin.IN, machine.Pin.PULL_UP)
 
 # define a function to handle button presses with protection against double-clicks
 last_button_press_time = 0
+last_button_pin = None
 
 def handle_button_press(pin):
     global last_button_press_time
+    global last_button_pin
     current_time = time.ticks_ms()
-    if current_time - last_button_press_time > 500: # ignore button presses that occur within 500 milliseconds of the last press
-        print('Button pressed!')
+    if current_time - last_button_press_time > 200: # ignore button presses that occur within 500 milliseconds of the last press
+        if last_button_pin == pin and current_time - last_button_press_time <= 1000: # check if the current button press is a double click
+            print('Double click detected!')
+        else:
+            print('Button pressed!')
+        last_button_pin = pin
         last_button_press_time = current_time
+
 
 # attach the button press handler function to the button pin
 button.irq(trigger=machine.Pin.IRQ_FALLING, handler=handle_button_press)
 
-# count up in seconds and print the current count
+# count up quick and print the current count to console to demonstrate running loop.
 count = 0
 while True:
     print(count)
     count += 1
-    time.sleep(1)
-
+    time.sleep(.2)
